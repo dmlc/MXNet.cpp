d2ab7d76c9957c1a5dc9ed018f9440043f5c4c21
Chuntao Hong <chuntao.hong@gmail.com>
2016-02-04 17:32:10 +0800

    remove getdep.sh since it may introduce strange bugs

--- example/getdep.sh
+++ example/getdep.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# usage: getdeps.sh binary_file
+
 echo getting dependency for $1
 mkdir lib 2> /dev/null
-for i in `ldd $1 | awk '{print $1}'`; do f=`locate -l 1 $i` && cp $f ./lib/; done
+ldd $1 | while read x; do
+	src=`echo $x | awk '{print $1}'`
+	dst=`echo $x | awk --field-separator='=>' '{if(NF==2) print $2; else print $1}' | awk '{print $1}'`
+	echo "${src} => ${dst}"
+	cp $dst ./lib/
+done
